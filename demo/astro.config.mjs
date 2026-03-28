17f4c7f6c373b820d4fffafa5029d36199b55b25
ryotahagihara <121647523+ryotahagihara@users.noreply.github.com>
2026-03-28 18:13:37 +0900

    chore: add tailwind, prettier, eslint

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from "astro/config";
 import contentCollectionsMinimal from "astro-content-collections-minimal";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [contentCollectionsMinimal()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
+  ...eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+]);
